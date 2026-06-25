a903d05be9853f596c11c472a0e85053f8dabf86
shivanid29 <shivanidesai2930@gmail.com>
2026-06-25 17:30:38 +0530

    chore: update email and password handling using env variables

--- utils/json_reader.py
+++ utils/json_reader.py
@@ -1,8 +1,27 @@
 import json
+import os
+from dotenv import load_dotenv
 
 
 def load_test_data():
 
-    with open("tests/test_data.json") as file:
+    # Load .env only for local machine
+    load_dotenv()
 
-        return json.load(file)
+    with open("tests/test_data.json") as file:
+        data = json.load(file)
+
+    email = os.getenv("EMAIL")
+    password = os.getenv("PASSWORD")
+
+    for test in data["test_cases"]:
+
+        if "payload" in test:
+
+            if "email" in test["payload"] and test["payload"]["email"] == "${EMAIL}":
+                test["payload"]["email"] = email
+
+            if "password" in test["payload"] and test["payload"]["password"] == "${PASSWORD}":
+                test["payload"]["password"] = password
+
+    return data
